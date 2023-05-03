89d6a518c60d883bc9ed9bd281dbf1c760706221
Maher Nassrallah <maher.ple2@gmail.com>
2023-05-03 21:27:40 +0300

    -

---
+++ src/components/Card/index.js
@@ -0,0 +1,8 @@
+const Card = () => {
+  return (
+    <div className="border-white border-2 w-40 h-40 rounded bg-[url('./public/BlueBird.png')]">
+      1
+    </div>
+  );
+};
+export default Card;
